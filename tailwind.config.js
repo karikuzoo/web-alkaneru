722b7a4ade18b24cc3a2749f3932c2f37b6eb9bd
karikuzoo <karkuzoo@gmail.com>
2022-04-28 13:31:11 +0700

    project web ali

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ['./public/**/*.{html,js}'],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#6EC1E4',
+        secondary: '#7A7A7A',
+      }
+    },
+  },
+  plugins: [],
+}
